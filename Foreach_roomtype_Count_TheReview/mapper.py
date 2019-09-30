46119d52dc025ed8d004487b6817c749d492fee5
Vamsee Krishna Gangapatnam <s533908@nwmissouri.edu>
2019-09-30 13:50:51 -0500

    update the changes

--- Foreach_roomtype_Count_TheReview/mapper.py
+++ Foreach_roomtype_Count_TheReview/mapper.py
@@ -5,8 +5,6 @@ r = open("mapperOutput.txt", "w")
 # read each line in input file
 lines = s.readlines()
 lines.sort()
-
-
 # out of all the columns in datasource select only room_type, and number_of_reviews columns as mapper output
 for line in lines:
     data = line.strip().split(',')
@@ -14,7 +12,6 @@ for line in lines:
         id,name,host_id,host_name,neighbourhood_group,neighbourhood,latitude,longitude,room_type,price,minimum_nights,number_of_reviews,last_review,reviews_per_month,calculated_host_listings_count,availability_365 = data
 
         r.write('{0}\t{1}\n'.format(room_type,number_of_reviews))
-
  
 s.close()
 r.close()
