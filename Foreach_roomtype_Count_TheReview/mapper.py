0c741f8675c25a42fb5a51d593a549ccef2b65d7
Vamsee Krishna Gangapatnam <s533908@nwmissouri.edu>
2019-09-30 22:54:14 -0500

    update modification

--- Foreach_roomtype_Count_TheReview/mapper.py
+++ Foreach_roomtype_Count_TheReview/mapper.py
@@ -12,6 +12,6 @@ for line in lines:
         id,name,host_id,host_name,neighbourhood_group,neighbourhood,latitude,longitude,room_type,price,minimum_nights,number_of_reviews,last_review,reviews_per_month,calculated_host_listings_count,availability_365 = data
 
         r.write('{0}\t{1}\n'.format(room_type,number_of_reviews))
- 
+        
 s.close()
 r.close()
--- Foreach_roomtype_Count_TheReview/reducer.py
+++ Foreach_roomtype_Count_TheReview/reducer.py
@@ -7,22 +7,29 @@ thisDict = {}
 # read each line in input file
 for line in s:
     data = line.strip().split('\t')
-    name = data[0]
-    year = data[1]
-    country = data[2]
+    room_type = data[0]
+    number_of_reviews = data[1]
+    
     # if country matches the previous country key in dictionary increment the key value with existing country value
-    if country in thisDict.keys():
-        thisDict[country] = thisDict[country] + 1
+    if room_type in thisDict.keys():
+        thisDict[room_type] = thisDict[room_type] +1
+        
     # if country doesnt match the previous country key in dictionary then consider key value as one
     else:
-        thisDict[country] = 1 
+        thisDict[room_type] = 1
        
 
 # print the output contents into reducerOutput.txt
+SU =0
 
 for keyItem,value in thisDict.items():
     r.write(keyItem +'\t'+ str(value) + "\n" )
     print(keyItem +'\t'+ str(value) +"\n" )
+    SU +=  value
+
+
+print(SU)
+   
 # close mapperOutput.txt and reducerOutput.txt
 s.close()
 r.close()
